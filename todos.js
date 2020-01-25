a71e012814c04d17324a7291143b58e9682a8544
Fabio Vige <fabiovige@gmail.com>
2020-01-25 14:05:47 -0300

    Renderizando Todos

--- todos.js
+++ todos.js
@@ -1,4 +1,4 @@
-var listElement = document.querySelector('#app li');
+var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
@@ -7,3 +7,15 @@ var todos = [
     'Estudar Javascript',
     'Acessar comunidade da Rocketseat',
 ];
+
+function renderTodos() {
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
